42ddfe50c60d8a59c9ebb6fe51c687cdc5097cdb
nao-kobayashi <nao.kobayashi@nifty.com>
2018-05-04 17:31:57 +0900

    fix to calculate large file.

--- file_checksum/src/main.rs
+++ file_checksum/src/main.rs
@@ -4,11 +4,14 @@ use std::env;
 use std::fs;
 use std::io::{BufReader, Read};
 
+//check windows
+//certutil -hashfile D:hoge.exe MD5
 fn main() {
     const BUF_SIZE: usize = 1048576;
     let args: Vec<String> = env::args().collect();
-    let mut result = Vec::new();
     let mut buffer: [u8; BUF_SIZE] = [0; BUF_SIZE];
+    let mut md5_context = md5::Context::new();
+    let mut calc_to = Vec::new();
 
     let mut reader = BufReader::with_capacity(BUF_SIZE, (fs::File::open(&args[1])).unwrap());
     loop { 
@@ -18,11 +21,13 @@ fn main() {
                     break;
                 }
 
+                calc_to.clear();
                 let mut count = 0;
                 while count < n {
-                    result.push(buffer[count]);
+                    calc_to.push(buffer[count]);
                     count += 1;
                 }
+                md5_context.consume(&calc_to);
             },
             Err(e) => {
                 println!("read error {:?}", e);
@@ -31,6 +36,6 @@ fn main() {
         }
     };
 
-    let digest = md5::compute(result);
+    let digest = md5_context.compute();
     println!("md5 is {:x}", digest);
 }
